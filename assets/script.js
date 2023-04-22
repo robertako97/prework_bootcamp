42d171f72030e3839203ef1e524f4f350d571a69
robertako97 <roberto.diazgmx@gmail.com>
2023-04-21 20:49:45 -0700

    JS

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,33 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
-    
- console.log(topics[x]);
+var topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topic[Math.floor(Math.random() * topic.length)];
+
+console.log('Here are the topics we learned through Prework:');
+Toppicz()
+
+function Toppicz() {
+    for (var x = 0; x < topic.length; x++) {
+        
+    console.log(topic[x]);
+    }
 }
 
-/*
+console.log('Which topic should we study first?');
 
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+SelectToppicz()
+
+function SelectToppicz() {
+
+    if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
+ 
 }
-*/
